87e527ce9ace99487abeab87ce91045327bfab41
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-27 22:34:40 +0000

    linted

--- eqArrays.js
+++ eqArrays.js
@@ -8,12 +8,12 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const eqArrays = function (arr1, arr2){
-  if( arr1.length !== arr2.length){
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
     return false;
-  }else {
-    for (let k = 0 ; k < arr1.length ; k++){
-      if (arr1[k] !== arr2[k] ) {
+  } else {
+    for (let k = 0; k < arr1.length; k++) {
+      if (arr1[k] !== arr2[k]) {
         return false;
       }
     }
@@ -27,11 +27,11 @@ const eqArrays = function (arr1, arr2){
 
 
 
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
