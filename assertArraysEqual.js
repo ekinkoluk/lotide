4ab8dbf6e8dd92bf4759d6944b1cf88bfae5ec32
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-27 22:38:41 +0000

    implemented an assert function for arrays

---
+++ assertArraysEqual.js
@@ -0,0 +1,27 @@
+const eqArrays = function (arr1, arr2){
+  if( arr1.length !== arr2.length){
+    return false;
+  }else {
+    for (let k = 0 ; k < arr1.length ; k++){
+      if (arr1[k] !== arr2[k] ) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (arr1, arr2){
+  if(eqArrays (arr1, arr2) === true){
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+  }
+  
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
