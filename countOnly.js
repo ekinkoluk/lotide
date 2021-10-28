7f0f8073ce24ebff8cc98b5c7c29734d0554b011
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-28 14:00:55 +0000

    lint

--- countOnly.js
+++ countOnly.js
@@ -15,18 +15,17 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   
-  for (const item of allItems){
+  for (const item of allItems) {
     
-      if(results[item] && itemsToCount[item]){ //undefined is false that's why it works
-        results[item] +=1;
-      }
-      else if (itemsToCount[item]){
-        results[item] = 1;
-      }
+    if (results[item] && itemsToCount[item]) { //undefined is false that's why it works
+      results[item] += 1;
+    } else if (itemsToCount[item]) {
+      results[item] = 1;
+    }
       
   }
   return results;
-}
+};
 
 
 const firstNames = [
@@ -42,7 +41,7 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }))
+console.log(countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }));
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
