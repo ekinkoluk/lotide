9a60667f00a28322a26330d0b89adf86c9e82930
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-28 14:05:19 +0000

    implemented countLetters

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const countLetters = function (sentence){
+  let results = {}
+  for (let k = 0; k < sentence.length; k++){
+    let char = sentence[k];
+    if(results[char]){
+      results[char] += 1;
+    } else{
+      results[char] = 1;
+    }
+  }
+  return results;
+};
+
+
+console.log (countLetters('ekiiin'));
+
