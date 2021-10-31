ec06cb67cf04ebd7ab134c021629949d60207178
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-31 15:31:19 +0000

    implemented take until function

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const takeUntil = function(array, callback){
+  for (let k = 0 ; k < array.length ; k++){
+        if(callback(array[k])) {
+          return array.slice(0,k);
+        }
+  }
+
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let k = 0; k < arr1.length; k++) {
+      if (arr1[k] !== arr2[k]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+  }
+  
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+
