dc1af804c57c53f1583a9228ad1e575b0b501b9d
Ekin Koluk <ekinkoluk@gmail.com>
2021-10-31 15:21:45 +0000

    implemented our map function

---
+++ map.js
@@ -0,0 +1,32 @@
+const map = function (arr, callback){
+    const results= [];
+    for (let item of arr){
+      results.push (callback(item));
+    }
+  return results;
+};
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let k = 0; k < arr1.length; k++) {
+      if (arr1[k] !== arr2[k]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("✅✅✅ Assertion Passed");
+  } else {
+    console.log("🛑🛑🛑 Assertion Failed");
+  }
+  
+};
+const words = ["ground", "control", "to", "major", "tom"];
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1,["g","c","t","m","t"]);
